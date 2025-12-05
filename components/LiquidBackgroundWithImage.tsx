0fcbb136e261b4a14d90e681160572f35fd7921b
Luis Bernard Ribeiro <info@wittywolf.pt>
2025-12-05 10:29:52 +0000

    Remove deprecated GooeyNav props

--- components/LiquidBackgroundWithImage.tsx
+++ components/LiquidBackgroundWithImage.tsx
@@ -200,11 +200,7 @@ export default function LiquidBackgroundWithImage() {
           items={navItems}
           particleCount={15}
           particleDistances={[90, 10]}
-          particleR={100}
-          initialActiveIndex={0}
           animationTime={600}
-          timeVariance={300}
-          colors={[1, 2, 3, 1, 2, 3, 1, 4]}
         />
       </div>
 
